eaaaa95d931cb91babcc22138ef868d8bb76059f
JeanChoi <jeanchoi3@gmail.com>
2014-11-22 16:32:20 -0500

    changed "hello" folder to rotationSchedule_app

--- rotationSchedule/settings.py
+++ rotationSchedule/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'rotationSchedule_app'
 )
 
 MIDDLEWARE_CLASSES = (
--- rotationSchedule/urls.py
+++ rotationSchedule/urls.py
@@ -3,15 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import rotationSchedule_app.views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'rotationSchedule.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', rotationSchedule_app.views.index, name='index'),
+    url(r'^db', rotationSchedule_app.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 
 )
