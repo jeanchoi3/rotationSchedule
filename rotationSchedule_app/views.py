99cb8eac38f1318191d6b9a138119f8a77eadf37
JeanChoi <jeanchoi3@gmail.com>
2014-12-01 17:22:04 -0500

    debug resident form not saving to database

--- rotationSchedule_app/views.py
+++ rotationSchedule_app/views.py
@@ -30,8 +30,10 @@ def detail(request, resident_id):
     if request.method == 'POST':
     	rForm = ResidentForm(request.POST)
     	if rForm.is_valid():
-    		temp_post = rForm.save(commit=False)
-    		temp_post.save()
+    		#temp_post = rForm.save(commit=False)
+    		#temp_post.save()
+    		newRes = Resident(name=rForm.cleaned_data['name'],year=rForm.cleaned_data['year'],track=rForm.cleaned_data['track'])
+            newRes.save()
     		return HttpResponseRedirect('/')
     else:
     	rForm = ResidentForm()
