b3e5f3cc1ca49600e5ceeadb9defcbf0bd804b12
JeanChoi <jeanchoi3@gmail.com>
2014-12-03 15:39:04 -0500

    add year model class to admin

--- rotationSchedule_app/admin.py
+++ rotationSchedule_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from rotationSchedule_app.models import Resident
+from rotationSchedule_app.models import Resident, Year
 
 # Register your models here.
 admin.site.register(Resident)
+admin.site.register(Year)
 
 class ResidentAdmin(admin.ModelAdmin):
     list_display = ('name','year','track','in_program')
