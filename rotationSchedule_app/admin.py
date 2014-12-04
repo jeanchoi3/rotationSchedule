16df8ba9c32b11f7c810460012ee0964d86d4b19
JeanChoi <jeanchoi3@gmail.com>
2014-12-04 13:56:30 -0500

    add rotation to admin site

--- rotationSchedule_app/admin.py
+++ rotationSchedule_app/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from rotationSchedule_app.models import Resident, Year, Track, Program
+from rotationSchedule_app.models import Resident, Year, Track, Program, Rotation
 
 # Register your models here.
 admin.site.register(Resident)
 admin.site.register(Year)
 admin.site.register(Track)
 admin.site.register(Program)
+admin.site.register(Rotation)
 
 class ResidentAdmin(admin.ModelAdmin):
     list_display = ('name','year','track','in_program')
