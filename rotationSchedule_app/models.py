34ca200466337910d2ab9e5b42b3e621554c1bb3
JeanChoi <jeanchoi3@gmail.com>
2014-12-04 22:31:57 -0500

    OLD TRACK REMOVED FROM RESIDENT MODEL

--- rotationSchedule_app/models.py
+++ rotationSchedule_app/models.py
@@ -3,10 +3,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-
-YEARS = (('PGY1','PGY1'),('PGY2','PGY2'),('PGY3','PGY3'),)
-TRACKS = (('track1','track1'),('track2','track2'),('track3','track3'),)
-
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
@@ -35,7 +31,6 @@ class Resident(models.Model):
         return self.name
     name = models.CharField(max_length=200)
     year = models.ForeignKey('Year', null=True) #each resident can only be in one year
-    #track = models.CharField(max_length=200,choices=TRACKS) #remove this later
     tracks = models.ManyToManyField('Track', null=True, default=None, related_name='tracks')
     inProgram = models.BooleanField(default=True)
 
