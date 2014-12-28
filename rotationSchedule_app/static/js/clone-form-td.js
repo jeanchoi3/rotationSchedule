9c9bd3c2628ee8f9cb8e86c8045d921d2c7f71e1
JeanChoi <jeanchoi3@gmail.com>
2014-12-27 22:49:39 -0600

    year cloning added

--- rotationSchedule_app/static/js/clone-form-td.js
+++ rotationSchedule_app/static/js/clone-form-td.js
@@ -102,7 +102,7 @@ $(function () {
                     if (num -1 === 1)
                 $('#btnDel').attr('disabled', true);
                 // enable the "add" button
-                $('#btnAdd').attr('disabled', false).prop('value', "add section");});
+                $('#btnAdd').attr('disabled', false).prop('value', "add block");});
             }
         return false; // Removes the last section you added
     });
@@ -152,7 +152,7 @@ $(function () {
                     if (num -1 === 1)
                 $('#btnDelRotation').attr('disabled', true);
                 // enable the "add" button
-                $('#btnAddRotation').attr('disabled', false).prop('value', "add section");});
+                $('#btnAddRotation').attr('disabled', false).prop('value', "add rotation");});
             }
         return false; // Removes the last section you added
     });
@@ -161,6 +161,49 @@ $(function () {
     // Disable the "remove" button
     $('#btnDelRotation').attr('disabled', true);
 
+
+    $('#btnAddYear').click(function () {
+        var num     = $('.clonedYear').length, // Checks to see how many "duplicatable" input fields we currently have
+            newNum  = new Number(num + 1),      // The numeric ID of the new input field being added, increasing by 1 each time
+            newElem = $('#year' + num).clone().attr('id', 'year' + newNum).fadeIn('slow'); // create the new element via clone(), and manipulate it's ID using newNum value
+
+        
+        //rotation name
+        newElem.find('.label_yn').attr('for', 'ID' + newNum + '_year_name');
+        newElem.find('.input_yn').attr('id', 'ID' + newNum + '_year_name').attr('name', 'ID' + newNum + '_year_name').val('');
+
+        // Insert the new element after the last "duplicatable" input field
+        $('#year' + num).after(newElem);
+        $('#ID' + newNum + '_title').focus();
+
+    // Enable the "remove" button. This only shows once you have a duplicated section.
+        $('#btnDelYear').attr('disabled', false);
+
+    // Right now you can only add 4 sections, for a total of 5. Change '5' below to the max number of sections you want to allow.
+        if (newNum == 20)
+        $('#btnAddYear').attr('disabled', true).prop('value', "You've reached the limit"); // value here updates the text in the 'add' button when the limit is reached 
+    });
+
+
+    $('#btnDelYear').click(function () {
+    // Confirmation dialog box. Works on all desktop browsers and iPhone.
+        if (confirm("Are you sure you wish to remove this section? This cannot be undone."))
+            {
+                var num = $('.clonedYear').length;
+                // how many "duplicatable" input fields we currently have
+                $('#year' + num).slideUp('slow', function () {$(this).remove();
+                // if only one element remains, disable the "remove" button
+                    if (num -1 === 1)
+                $('#btnDelYear').attr('disabled', true);
+                // enable the "add" button
+                $('#btnAddYear').attr('disabled', false).prop('value', "add section");});
+            }
+        return false; // Removes the last section you added
+    });
+    // Enable the "add" button
+    $('#btnAddYear').attr('disabled', false);
+    // Disable the "remove" button
+    $('#btnDelYear').attr('disabled', true);
 
 
 });
